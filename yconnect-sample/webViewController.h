e48b02257bb4edea7e928e13abf96cc89a45033d
jlai <result0924@gmail.com>
2018-02-06 15:02:11 +0800

    Add webViewController.h/m/xib

---
+++ yconnect-sample/webViewController.h
@@ -0,0 +1,13 @@
+//
+//  webViewController.h
+//  yconnect-sample
+//
+//  Created by lai Kuan-Ting on 2018/2/6.
+//  Copyright © 2018年 hanksudo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface webViewController : UIViewController
+
+@end
---
+++ yconnect-sample/webViewController.m
@@ -0,0 +1,37 @@
+//
+//  webViewController.m
+//  yconnect-sample
+//
+//  Created by lai Kuan-Ting on 2018/2/6.
+//  Copyright © 2018年 hanksudo. All rights reserved.
+//
+
+#import "webViewController.h"
+
+@interface webViewController ()
+
+@end
+
+@implementation webViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
